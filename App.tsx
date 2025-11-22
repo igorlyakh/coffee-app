60d1dfaef475c9f10e29e1878ab02de259daa959
Igor Liakh <igorlyah060@gmail.com>
2025-11-22 16:02:10 +0200

    layoyt done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <View style={styles.textBlock}>
+          <Text style={styles.header}>Одно из самых вкусных кофе в городе!</Text>
+          <Text style={styles.text}>
+            Свежие зёрна, настоящая арабика и бережная обжарка
+          </Text>
+        </View>
+        <Button title="Начать" />
+      </View>
     </View>
   );
 }
@@ -13,8 +19,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+    backgroundColor: '#000000',
+  },
+  content: {
+    gap: 24,
+  },
+  textBlock: {
+    gap: 8,
+  },
+  header: {
+    fontSize: 34,
+    fontWeight: 600,
+    textAlign: 'center',
+    color: '#FFFFFF',
+    width: 315,
+    letterSpacing: 0.34,
+  },
+  text: {
+    fontSize: 14,
+    letterSpacing: 0.14,
+    color: '#A9A9A9',
+    textAlign: 'center',
+    lineHeight: 22,
   },
 });
